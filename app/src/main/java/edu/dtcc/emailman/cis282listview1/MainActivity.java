474476e116d169131dd552a96e3b08f88fd8bb3f
emailman <emailman@dtcc.edu>
2016-02-27 11:34:31 -0500

    Initial commit

---
+++ app/src/main/java/edu/dtcc/emailman/cis282listview1/MainActivity.java
@@ -0,0 +1,62 @@
+package edu.dtcc.emailman.cis282listview1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get the array for the list
+        final String[] fruitsArray = getResources().getStringArray(R.array.fruits);
+
+        // Get a reference to the text view
+        final TextView selectedOpt = (TextView) findViewById(R.id.selectedopt);
+
+        // Get a reference to the list view
+        ListView fruitsList = (ListView) findViewById(R.id.fruitslist);
+
+        // Setup a listener on the list
+        fruitsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedOpt.setText("You selected " + fruitsArray[position]);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
